15bfae924454c15f8b18debd9ec3c90b026589b3
Matthew Trepte <matthewtrepte@calvisitor-10-105-184-211.calvisitor.1918.berkeley.edu>
2017-04-30 02:08:08 -0700

    added cross validation

--- model_test.py
+++ model_test.py
@@ -1,5 +1,5 @@
 from featurizer import get_tweets, get_feats_and_labels, tfidf, bag_of_words
-from models import PCA, knn, random_forest
+from models import PCA, knn, random_forest, cross_validate
 
 tweets = get_tweets()
 data = get_feats_and_labels(tweets)
@@ -10,5 +10,9 @@ cont_labels = data[2]
 print("num of features: ", len(features))
 
 # PCA(features, bin_labels)
-print("score: ", knn(features, bin_labels))
-# print("score: ", random_forest(features, bin_labels))
+
+print("knn:")
+cross_validate(knn, features, bin_labels)
+
+print("random forest:")
+cross_validate(random_forest, features, bin_labels)
--- models.py
+++ models.py
@@ -1,24 +1,31 @@
 from sklearn.decomposition import PCA as sklearn_PCA
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
 import math
-# from keras.models import Sequential
-# from keras.layers import Dense
-# from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
-def PCA(X, y):
+
+def cross_validate(model, X, y):
+	folds = 10
+
+	# print scores for each fold
+	for fold in range(folds):
+		score = model(X, y, fold, folds)
+		print(fold, score)
+
+
+def PCA(X, y, fold, folds):
 	# hyperparameters
 	n_dim = 2
 	
-	# label color mapping
-	labels = ['hate', 'chill']
-	colors = ['red', 'blue']
+	# color mapping
+	labels = ['chill', 'hate']
+	colors = ['blue', 'red']
 
-	
 	# apply PCA to featurized tweets
 	pca_model = sklearn_PCA(n_components=n_dim)
 	X = pca_model.fit_transform(X)
@@ -37,30 +44,43 @@ def PCA(X, y):
 
 	plt.show()
 
-def knn(X, y):
-	# partition dataset
-	training_percent = .70
-	X_train, y_train, X_test, y_test = partition(X, y, training_percent)
-	
+def knn(X, y, fold, folds):
 	# hyperparameters
 	n_neighbors = 5
-	
+	n_components = 100 # vec dim unrreduced is 28113
+
+	print("fold: ", fold)
+
+	print("PCA-ing")
+
+	# reduce dimensions of vectors to expedite prediction and feature select
+	pca_model = sklearn_PCA(n_components=n_components)
+	X = pca_model.fit_transform(X)
+
+	print("partitioning")
+
+	# partition dataset
+	X_train, y_train, X_test, y_test = partition(X, y, fold, folds)
+
+	print("training")
+
 	# train
 	knn_model = KNeighborsClassifier(n_neighbors=n_neighbors)
 	knn_model.fit(X_train, y_train)
 
-	# computer accuracy
+	print("score")
+
+	# compute accuracy
 	score = knn_model.score(X_test, y_test)
 
 	return score
 
-def random_forest(X, y):
-	# partition dataset
-	training_percent = .70
-	X_train, y_train, X_test, y_test = partition(X, y, training_percent)
-	
+def random_forest(X, y, fold, folds):
 	# hyperparameters
 	forests_num = 20
+
+	# partition dataset
+	X_train, y_train, X_test, y_test = partition(X, y, fold, folds)
 	
 	# train
 	random_forest = RandomForestClassifier(n_estimators=forests_num)
@@ -68,39 +88,22 @@ def random_forest(X, y):
 
 	# computer accuracy
 	score = random_forest.score(X_test, y_test)
-	
+
 	return score
 
-def partition(X, y, training_percent):
-	partition_index = int(training_percent * len(X))
-	X_train = X[:partition_index]
-	y_train = y[:partition_index]
-	X_test = X[partition_index:]
-	y_test = y[partition_index:]
+def partition(X, y, fold, num_folds):
+	partition_start = int(fold / num_folds * len(X))
+	partition_end = int((fold + 1) / num_folds * len(X))
+
+	X_train = X[partition_start:partition_end]
+	y_train = y[partition_start:partition_end]
+
+	print(type(X))
+	X = list(X)
+	X_test = np.array(X[:partition_start] + X[partition_end:])
+	y = list(y)
+	y_test = np.array(y[:partition_start] + y[partition_end:])
+
+	print(len(X_train), len(y_train), len(X_test), len(y_test))
 
 	return [X_train, y_train, X_test, y_test]
-
-# def rnn(X, y):
-#	 #hyperparameters
-#	 look_back = 1
-	
-#	 # normalizing featurized tweets
-#	 scaler = MinMaxScaler(feature_range=(0, 1))
-#	 X = scalar.fit_transform(X)
-	
-#	 # partition dataset
-#	 training_percent = .70
-#	 partition_index = int(training_percent * len(X))
-#	 X_train = X[:partition_index]
-#	 X_test = X[partition_index:]
-#	 y_train = y[:partition_index]
-#	 y_test = y[partition_index:]
-	
-# # convert an array of values into a dataset matrix
-# def create_dataset(dataset, look_back=1):
-#	 dataX, dataY = [], []
-#	 for i in range(len(dataset)-look_back-1):
-#		 a = dataset[i:(i+look_back), 0]
-#		 dataX.append(a)
-#		 dataY.append(dataset[i + look_back, 0])
-#	 return numpy.array(dataX), numpy.array(dataY)
